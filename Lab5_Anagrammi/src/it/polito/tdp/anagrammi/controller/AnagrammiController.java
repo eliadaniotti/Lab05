d45dbe2e3f1dfc9c9afa55a86274db14a67cbb0e
Elia Daniotti <Elia Daniotti@DESKTOP-55HFKSK>
2019-06-14 17:13:07 +0200

    ricorsione boh

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
@@ -1,8 +1,49 @@
 package it.polito.tdp.anagrammi.controller;
 
+import java.util.List;
+
+import it.polito.tdp.anagrammi.model.Model;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AnagrammiController {
-
 	
+	Model model= new Model();
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnCalcola;
+
+    @FXML
+    private TextArea txtCorretti;
+
+    @FXML
+    private TextArea txtErrati;
+
+    @FXML
+    private Button btnReset;
+    
+    public void setModel(Model model) {
+    	this.model=model;
+    }
+    
+    @FXML
+    void doCalcola(ActionEvent event) {
+    	String parola = txtParola.getText();
+    	List<String> anagrammi = model.getAnagrammi(parola);
+    	for(String s : anagrammi)
+    		txtCorretti.appendText(s + "\n");
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
 }
+
 
